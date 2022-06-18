95f25740f2c045adac4e2d3165b332b40f76d82f
heewon916 <kimhiwon@naver.com>
2022-06-19 01:39:24 +0900

    install pymysql; modify settings.py-import mysql;DATABASES;

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+##---edit
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +81,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stock_test',
+        'USER': 'root',
+        'PASSWORD': 'hkim916!@',
+        'HOST': '',
+        'PORT':'3306'
     }
 }
 
