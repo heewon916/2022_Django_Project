d14042b0a35592e89249777562e0c281ecdb7efd
heewon916 <kimhiwon@naver.com>
2022-06-19 00:51:27 +0900

    create superuser; add data samsung at companys; add search func.;

--- mysite/stock/admin.py
+++ mysite/stock/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Company
 # Register your models here.
+
+# 검색 기능 추가
+class CompanyAdmin(admin.ModelAdmin):
+    search_fields = ['company']
+
+admin.site.register(Company, CompanyAdmin)
--- mysite/stock/models.py
+++ mysite/stock/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
+from re import T
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    code = models.CharField(max_length=20, primary_key=True)
+    company = models.CharField(max_length=40)
+    last_update = models.DateField()
+    
+    def __str__(self):
+        return self.company
