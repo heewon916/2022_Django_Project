8effbdcfd7a0351146d88dbb06ce277373a94048
heewon916 <kimhiwon@naver.com>
2022-06-19 01:30:27 +0900

    create index.html; print datas in company_list using index.html;

--- mysite/stock/views.py
+++ mysite/stock/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 ##--edit
 from django.http import HttpResponse
+
+from .models import Company
 # Create your views here.
 
 def index(request):
-    return HttpResponse("안녕하세요, 기본 페이지입니다.")
+    #edit
+    company_list = Company.objects.all() #Company 모델에 있는 정보를 전부 가져와서
+    context = {'company_list': company_list} #company_list의 정보를 context에 담는다. 
+    return render(request, 'stock/index.html', context) # company에 있는 정보를 모두 가져와서, index.html파일로 넘겨줄 것이다. 
